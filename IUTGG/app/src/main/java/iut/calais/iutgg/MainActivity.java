c546dacaaad150edf5462c70f442b17e03ceb221
comellobaptiste <baptiste.comello@gmail.com>
2019-03-12 17:05:40 +0100

    Test #1 - Fragment

--- IUTGG/app/src/main/java/iut/calais/iutgg/MainActivity.java
+++ IUTGG/app/src/main/java/iut/calais/iutgg/MainActivity.java
@@ -1,44 +1,78 @@
-package iut.calais.iutgg;
+package net.simplifiedcoding.iutgg;
 
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
-import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import net.simplifiedcoding.bottomnavigationexample.JoueurFragment;
 
-    private TextView mTextMessage;
+import iut.calais.iutgg.EquipeFragment;
+import iut.calais.iutgg.HomeFragment;
+import iut.calais.iutgg.MatchFragment;
+import iut.calais.iutgg.R;
+import iut.calais.iutgg.TournoisFragment;
 
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+//implement the interface OnNavigationItemSelectedListener in your activity class
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
 
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
-                    return true;
-            }
-            return false;
-        }
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        //loading the default fragment
+        loadFragment(new HomeFragment());
+
+        //getting bottom navigation view and attaching the listener
+        BottomNavigationView navigation = findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(this);
     }
 
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Fragment fragment = null;
+
+        switch (item.getItemId()) {
+            case R.id.navigation_home:
+                fragment = new HomeFragment();
+                break;
+
+            case R.id.navigation_profile:
+                fragment = new EquipeFragment();
+                break;
+
+            case R.id.navigation_notifications:
+                fragment = new JoueurFragment();
+                break;
+
+        /*    case R.id.navigation_:
+                fragment = new MatchFragment();
+                break;
+
+            case R.id.navigation_profile:
+                fragment = new TournoisFragment();
+                break;*/
+        }
+
+
+
+        return loadFragment(fragment);
+    }
+
+    private boolean loadFragment(Fragment fragment) {
+        //switching fragment
+        if (fragment != null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
 }
